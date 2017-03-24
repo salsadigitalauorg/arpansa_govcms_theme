511a8d1f524eb3797b24ea00167ed70b8bd043ad
Nathan Perry <nathan@salsadigital.com.au>
2017-03-24 17:17:12 +1300

    ARPANSA-103: Expand front-end approach of separating the two header logos and add theme setting for adding secondary logo details.

--- arpansa_theme/theme-settings.php
+++ arpansa_theme/theme-settings.php
@@ -9,6 +9,55 @@
  * Implements hook_system_theme_settings_alter().
  */
 function arpansa_theme_form_system_theme_settings_alter(&$form, $form_state) {
+  // I want to place the Secondary Logo enable checkbox underneath the Logo enable checkbox as it makes sense.
+  $new_theme_settings = array();
+
+  foreach ($form['theme_settings'] as $key => $setting) {
+    $new_theme_settings[$key] = $setting;
+    if ($key == 'toggle_logo') {
+      $new_theme_settings['toggle_secondary_logo'] = array(
+        '#type' => 'checkbox',
+        '#title' => 'Secondary Logo',
+        '#default_value' => theme_get_setting('toggle_secondary_logo'),
+      );
+    }
+  }
+
+  $form['theme_settings'] = $new_theme_settings;
+
+  $form['secondary_logo'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Secondary Logo'),
+    '#weight' => 2,
+    '#collapsible' => FALSE,
+    '#collapsed' => FALSE,
+  );
+
+  $form['secondary_logo']['secondary_logo_path'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Path to secondary logo'),
+    '#description'   => t("The path to the file you would like to use as your secondary logo file."),
+    '#default_value' => theme_get_setting('secondary_logo_path'),
+  );
+
+  $form['secondary_logo']['secondary_logo_upload'] = array(
+    '#type'          => 'managed_file',
+    '#title'         => t('Upload secondary logo image'),
+    '#description'   => t("If you don't have direct file access to the server, use this field to upload your secondary logo."),
+    '#upload_location' => file_default_scheme() . '://theme/',
+    '#default_value' => theme_get_setting('secondary_logo_upload'),
+    '#upload_validators' => array(
+      'file_validate_extensions' => array('gif png jpg jpeg')
+    ),
+  );
+
+  $form['secondary_logo']['secondary_logo_alt'] = array(
+    '#type'          => 'textfield',
+    '#title'         => t('Secondary logo alternative text'),
+    '#description'   => t("Alternative text to assign to the secondary logo in the top header."),
+    '#default_value' => theme_get_setting('secondary_logo_alt'),
+  );
+
   $form['govcms_ui_kit_options'] = array(
     '#type' => 'fieldset',
     '#title' => t('govCMS UI Kit settings'),
