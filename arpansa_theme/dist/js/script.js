ed8d58b61c5a43451859b9bdf31e81fc9714a861
Bondan Sentanu <bondan@salsadigital.com.au>
2017-04-13 11:23:54 +0800

    [ARPANSA-69] Sumoselect js only applied to multiselect

--- arpansa_theme/dist/js/script.js
+++ arpansa_theme/dist/js/script.js
@@ -704,7 +704,7 @@ var desktop_column = 1170;
       }
 
       // Sumoselect all select
-      $('.form-select', context).SumoSelect();
+      $('select.form-select[multiple=multiple]', context).SumoSelect();
     }
   };
 
