ed2e2c21e9526e15bea7cc11afa01a768c1d2ac3
Sonny Kieu <sonny@salsadigital.com.au>
2017-06-01 14:55:56 +1000

    Prepare for theme subtree.

--- docroot/sites/default/themes/site/arpansa_theme/templates/webform-calendar.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Theme the button for the date component date popup.
- */
-?>
-<input aria-label="<?php print t('Open popup calendar'); ?>" type="button" class="<?php print implode(' ', $calendar_classes); ?>" title="<?php print t('Open popup calendar'); ?>" />
