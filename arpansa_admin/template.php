85ff01d65f45bf313afad5f8e574873abb07f176
Sonny Kieu <sonny@salsadigital.com.au>
2017-06-01 14:58:17 +1000

    Theme subtree.

---
+++ arpansa_admin/template.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * Contains the theme's functions to manipulate Drupal's admin default markup.
+ */
+
+/**
+ * Implements template_preprocess_views_view_row_rss().
+ *
+ * @see template_preprocess_views_view_row_rss()
+ */
+function arpansa_admin_preprocess_views_view_row_rss(&$variables) {
+  $view = $variables['view'];
+  $row = $variables['row'];
+  if ($view->name == 'what_s_new' && $view->current_display == 'feed_whats_new') {
+    // Use Consultation Summary for RSS row description [ARPANSA-79].
+    $current_result = $view->result[$variables['id'] - 1];
+    if (!empty($current_result->field_field_consultation_summary[0]['rendered']['#markup'])) {
+      $row->description = strip_tags($current_result->field_field_consultation_summary[0]['rendered']['#markup']);
+      $variables['description'] = $row->description;
+    }
+  }
+}
+
+/**
+ * Implements hook_media_wysiwyg_token_to_markup_alter().
+ *
+ * @see media_wysiwyg_token_to_markup()
+ */
+function arpansa_admin_media_wysiwyg_token_to_markup_alter(&$element, $tag_info, $settings) {
+  if (!empty($element['content'])) {
+    unset($element['content']['#type']);
+    unset($element['content']['#attributes']);
+    $element['content']['#file']->media_markup = TRUE;
+  }
+}
+
+/**
+ * Implements template_preprocess_file_entity().
+ *
+ * @see template_preprocess_file_entity()
+ */
+function arpansa_admin_preprocess_file_entity(&$variables) {
+  if (!empty($variables['media_markup'])) {
+    $variables['theme_hook_suggestions'][] = 'file_entity__inline_wysiwyg';
+    if (!empty($variables['field_document_title'][LANGUAGE_NONE][0]['value'])) {
+      $document_title = $variables['field_document_title'][LANGUAGE_NONE][0]['value'];
+      $variables['content']['file']['#file']->description = $document_title;
+    }
+  }
+}
+
+/**
+ * Implements hook_entity_view_alter().
+ */
+function arpansa_admin_entity_view_alter(&$build, $type) {
+  if ($type == 'file') {
+    unset($build['links']);
+  }
+}
+
+/**
+ * Implements theme_file_icon().
+ */
+function arpansa_admin_file_icon($variables) {
+  $file = $variables['file'];
+  $alt = $variables['alt'];
+  $icon_directory = $variables['icon_directory'];
+
+  $mime = check_plain($file->filemime);
+  if ($mime == 'application/rtf') {
+    $file->filemime = 'application/msword';
+  }
+  $icon_url = file_icon_url($file, $icon_directory);
+
+  return '<img class="file-icon" alt="' . check_plain($alt) . '" title="' . $mime . '" src="' . $icon_url . '" />';
+}
